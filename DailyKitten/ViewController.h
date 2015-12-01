09d097af92520c77cf7d7ba4dd84c941c9211406
MengjueW <mw856@cornell.edu>
2015-11-30 20:35:57 -0500

    Initial Commit

---
+++ DailyKitten/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DailyKitten
+//
+//  Created by Mengjue Wang on 11/30/15.
+//  Copyright © 2015 Mengjue Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
